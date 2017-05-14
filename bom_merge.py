85d07cad609e97a17643aa015231b083465edd34
Damian Pala <mail@damianpala.com>
2017-05-14 21:40:18 +0200

    New merged bom file name creation implemented.

--- bom_merge.py
+++ bom_merge.py
@@ -1,33 +1,42 @@
 '''
-@brief 	    Script to mergeing BOM to one file.
+@brief 		Script to mergeing BOM to one file.
 		
-@author     Damian Pala
-@date       19.01.2017
+@author	 Damian Pala
+@date	   19.01.2017
 '''
 
 import sys
 import os
 from MyEagleBOM import Bom, ExportBom, MergeCsvFilesToOneObjectNoRowMerge, OpenCsvFile, InputFileType, show_exception_and_exit
 
+def create_new_bom_filename(input_files_list):
+	new_file_name = ""
+	for item in input_files_list:
+		new_file_name += os.path.splitext(os.path.basename(item))[0] + "_"
+		
+	new_file_name += "merged_bom.csv"
+	
+	return new_file_name
+
 if __name__ == "__main__":
-    sys.excepthook = show_exception_and_exit
-    fileName = sys.argv[1:]
-    scritDirectory = os.path.dirname(sys.argv[0])
+	sys.excepthook = show_exception_and_exit
+	fileName = sys.argv[1:]
+	scritDirectory = os.path.dirname(sys.argv[0])
 
-#     fileName.append("bom1.csv")
-#     fileName.append("bom2.csv")
+#	 fileName.append("bom1.csv")
+#	 fileName.append("bom2.csv")
 
-    if len(fileName) == 1:
-        csvFile = OpenCsvFile(fileName[0])
-        bom = Bom(csvFile)
-        bom.CreateBom(InputFileType.BOM_FILE)
-        exportBom = ExportBom(bom)
-        exportBom.WriteCsv("Bom")
-    elif len(fileName) > 1:
-        csvFile = MergeCsvFilesToOneObjectNoRowMerge(fileName)
-        bom = Bom(csvFile)
-        bom.CreateBom(InputFileType.BOM_FILE)
-        exportBom = ExportBom(bom)
-        exportBom.WriteCsv("Bom")
-    else:
-        """Do nothing"""
+	if len(fileName) == 1:
+		csvFile = OpenCsvFile(fileName[0])
+		bom = Bom(csvFile)
+		bom.CreateBom(InputFileType.BOM_FILE)
+		exportBom = ExportBom(bom)
+		exportBom.WriteCsv(create_new_bom_filename(fileName))
+	elif len(fileName) > 1:
+		csvFile = MergeCsvFilesToOneObjectNoRowMerge(fileName)
+		bom = Bom(csvFile)
+		bom.CreateBom(InputFileType.BOM_FILE)
+		exportBom = ExportBom(bom)
+		exportBom.WriteCsv(create_new_bom_filename(fileName))
+	else:
+		"""Do nothing"""
